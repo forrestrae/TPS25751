39b89784936ad8f50c11db1c21a4f139853add37
Forrest Rae <fbr@14x.net>
2026-06-18 16:41:13 -0700

    refactor: remove inconsistent indentation and trailing whitespace in TPS25751Mode header file

--- include/TPS25751Mode.h
+++ include/TPS25751Mode.h
@@ -3,7 +3,7 @@
 #include "TPS25751Register.h"
 #include "TPS25751RegisterAddress.h"
 
-    enum class ModeType : uint8_t {
+enum class ModeType : uint8_t {
     Application = 0,
     Boot = 1,
     Patch = 2,
@@ -20,7 +20,7 @@ public:
 
 
     const char *modeString() const;
-    
+
     ModeType getModeType() const;
 
     bool isApplicationMode() const;
@@ -30,7 +30,7 @@ public:
     bool isPatchMode() const;
 
     void debugPrint(Stream &s = Serial) const override;
-    
+
     /**
      * @brief Validates mode register data semantically
      * @return true if mode data is valid according to TPS25751 specification
